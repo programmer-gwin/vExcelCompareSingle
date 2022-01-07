78750882ef121b3d386aac3c0c5a6b86431e2aa6
GWINA <babsgodwin@gmail.com>
2022-01-07 15:31:46 +0100

    first commit

---
+++ vExcelCompare/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace vExcelCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTotalFailed = new System.Windows.Forms.Label();
+            this.lblTotalSuccess = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 8);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(668, 666);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(686, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "EXECUTE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Navy;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(686, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ADD FILE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(709, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sheet Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(686, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Sheet1";
+            // 
+            // lblTotalFailed
+            // 
+            this.lblTotalFailed.AutoSize = true;
+            this.lblTotalFailed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalFailed.Location = new System.Drawing.Point(764, 309);
+            this.lblTotalFailed.Name = "lblTotalFailed";
+            this.lblTotalFailed.Size = new System.Drawing.Size(115, 24);
+            this.lblTotalFailed.TabIndex = 8;
+            this.lblTotalFailed.Text = "Sheet Name";
+            this.lblTotalFailed.Click += new System.EventHandler(this.lblTotalFailed_Click);
+            // 
+            // lblTotalSuccess
+            // 
+            this.lblTotalSuccess.AutoSize = true;
+            this.lblTotalSuccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSuccess.Location = new System.Drawing.Point(764, 393);
+            this.lblTotalSuccess.Name = "lblTotalSuccess";
+            this.lblTotalSuccess.Size = new System.Drawing.Size(115, 24);
+            this.lblTotalSuccess.TabIndex = 9;
+            this.lblTotalSuccess.Text = "Sheet Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1308, 686);
+            this.Controls.Add(this.lblTotalSuccess);
+            this.Controls.Add(this.lblTotalFailed);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel File Comaparison (ETL)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTotalFailed;
+        private System.Windows.Forms.Label lblTotalSuccess;
+    }
+}
+
---
+++ vExcelCompare/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace vExcelCompare
+{
+    public partial class Form1 : Form
+    {
+        DataTable src1, src2;
+        bool IsMakeSameLength;
+        DataTable src3 = null;
+        int i;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetDefaultTextColor();
+
+                src3 = new DataTable();
+                i = 0;
+
+                SetupForTable1();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+        }
+
+        private async void SetupForTable1()
+        {
+           // int index = 0;
+            foreach (DataRow row1 in src1.Rows)
+            {
+                string row1Data = row1.ItemArray[0].ToString();
+                //await CallAPIforResponse("2033JUS2");
+                await CallAPIforResponse(row1Data);
+               // index++;
+            }
+            sucess = 0; failed = 0;
+
+            if (NotSuccessfulTID.Count != 0)
+            {
+                foreach (string row1Data in NotSuccessfulTID)
+                {
+                    await CallAPIforResponse(row1Data);
+                }
+            }
+            sucess = 0; failed = 0;
+        }
+
+        List<string> NotSuccessfulTID = new List<string>();
+        int sucess = 0; int failed = 0;
+        private async Task CallAPIforResponse(string row1Data)
+        {
+            //https://capriconiosapi.elkanahtech.com/MPOSAPI/DownloadParameterByTID?TID=2033JYO9
+            var modelV = new { TID = row1Data };
+            string json = JsonConvert.SerializeObject(modelV);
+            string res = await PostAPI("DownloadParameterByTID", json);
+            if (string.IsNullOrEmpty(res) || res.Contains("{\"ResponseCode\": \"00")) { NotSuccessfulTID.Add(row1Data); failed += 1; }
+            else sucess += 1;
+
+            lblTotalFailed.Text = "Failed: " + failed.ToString();
+            lblTotalSuccess.Text = "Success: " + sucess.ToString();
+        }
+
+        public static async Task<string> PostAPI(string actionName, string mrawData)
+        {
+            string mresult = "";
+            try
+            {
+                using (var mclient = new HttpClient() { BaseAddress = new Uri("https://capriconiosapi.elkanahtech.com/MPOSAPI/") })
+                {
+                    // mclient.Timeout = TimeSpan.FromMinutes(1);
+                    var response = await mclient.PostAsync($"{actionName}/", new StringContent(mrawData, Encoding.UTF8, "application/json")).ConfigureAwait(false);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                mresult = "";
+            }
+            return mresult;
+        }
+
+        private void SetDefaultTextColor()
+        {
+            for (int a = 0; a < dataGridView1.RowCount - 1; a++)
+            {
+                dataGridView1.Rows[a].DefaultCellStyle.ForeColor = Color.Black;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoResizeColumns();
+        }
+
+        string fileName = "";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadExcel(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadExcel(2);
+        }
+
+        private void LoadExcel(int p)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();  //create openfileDialog Object
+                openFileDialog1.Filter = "XML Files (*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb) |*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb";//open file format define Excel Files(.xls)|*.xls| Excel Files(.xlsx)|*.xlsx| 
+                openFileDialog1.FilterIndex = 3;
+
+                openFileDialog1.Multiselect = false;        //not allow multiline selection at the file selection level
+                openFileDialog1.Title = "Open Text File-R13";   //define the name of openfileDialog
+                openFileDialog1.InitialDirectory = @"Desktop"; //define the initial directory
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)        //executing when file open
+                {
+                    string pathName = openFileDialog1.FileName;
+                    fileName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                    string sheetName = "Sheet1";
+                    if (p == 1)
+                    {
+                        src1 = new DataTable();
+                        sheetName = textBox1.Text;
+                    }
+
+                    string strConn = string.Empty;
+                    FileInfo file = new FileInfo(pathName);
+                    if (!file.Exists) { throw new Exception("Error, file doesn't exists!"); }
+                    string extension = file.Extension;
+                    switch (extension)
+                    {
+                        case ".xls":
+                            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                            break;
+                        case ".xlsx":
+                            strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + pathName + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1;'";
+                            break;
+                        default:
+                            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                            break;
+                    }
+                    OleDbConnection cnnxls = new OleDbConnection(strConn);
+                    OleDbDataAdapter oda = new OleDbDataAdapter(string.Format("select * from [{0}$]", sheetName), cnnxls);
+                    if (p == 1)
+                    {
+                        oda.Fill(src1);
+                        dataGridView1.DataSource = src1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!" + ex.Message);
+            }
+        }
+
+        private void lblTotalFailed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
